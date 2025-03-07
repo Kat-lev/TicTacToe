0cfd33ec85c379d85ce85f9bfc67a98e16811c44
saudlari <larissa@riseup.net>
2025-03-07 14:55:19 +0100

    create draft structure

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,36 @@
 public class TicTacToe{
+
     public static void main(String[] args){
         System.out.println("Holi esto va a ser nuestro juegooo!");
     }
 }
+
+//Declarar la matriz (tablero)
+
+// Creamos el tablero y imprimimos  
+
+// Crear guion bajo tablero
+
+// Crear un mensaje de normas. Imprimir este mensaje al iniciar el juego. 
+
+// Crear la función de ganar. 
+
+// Crear función de empate despues de 9 turnos
+
+// Crear el scanner para recopilar el turno de x
+
+// Comprobar si lo que pide x está correcto. Mensaje de error. Ejemplo: Poner x fuera del juego y verificar si el espacio ya está ocupado. 
+
+// Imprimir X en el lugar que tiene que estar en el tablero
+
+// Comprobar con el condicional si el jugador ha ganado
+
+// Si no, rotar turno a O 
+
+// Comprobar si lo que pide 0 está correcto. Mensaje de error: Poner 0 fuera del juego y verificar si el espacio ya está ocupado.
+
+ // Imprimir 0 en el lugar que tiene que estar en el tablero
+
+// Comprobar con el condicional si el jugador ha ganado
+
+// Despues de 9 turnos, llamar la función de empate. 
