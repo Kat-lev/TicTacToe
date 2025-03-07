e242bf13f566b3cb04790cab7c04e6d5884e629b
kat-lev <kat@katleverton.com>
2025-03-07 15:28:25 +0100

    add empty board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,38 @@
 public class TicTacToe{
 
+private static char[][] board = new char [3][3];
     public static void main(String[] args){
+        createBoard();
+        printBoard();
         System.out.println("Holi esto va a ser nuestro juegooo!");
+    }
+
+    private static void createBoard(){
+        for (int row = 0; row < 3; row++){
+            for (int column = 0; column < 3; column++){
+                board[row][column] = '_';
+            }
+        }
+    }
+
+    private static void printBoard(){
+        System.out.println("\n");
+        for (int row = 0; row < 3; row++) {
+            System.out.print(" ");
+            for (int column = 0; column < 3; column++) {
+                System.out.print(board[row][column]  + " ");
+            }
+            System.out.println();
+            System.out.println("\n");
+        }
     }
 }
 
-//Declarar la matriz (tablero)
+//Declarar la matriz (tablero) OK
 
-// Creamos el tablero y imprimimos  
+// Creamos el tablero y imprimimos OK
 
-// Crear guion bajo tablero
+// Crear guion bajo tablero OK
 
 // Crear un mensaje de normas. Imprimir este mensaje al iniciar el juego. 
 
